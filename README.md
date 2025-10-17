852d584368d403f46041770fde0b174e9e6fbeec
rposts <rposts>
2025-10-17 18:45:54 +0000

    Add build files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Terraform
+Terraform is a tool for building, changing, and versioning infrastructure safely and efficiently. Terraform can manage existing and popular service providers as well as custom in-house solutions.
+
+Additional information available [here](https://github.com/hashicorp/terraform).
---
+++ src/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -o pipefail
+read -ra arr <<< "$@"
+version=${arr[1]}
+trap 0 1 2 ERR
+# Ensure sudo is installed along with utilities to build rpm and deb packages
+apt-get update && apt-get install sudo ruby ruby-dev rpm -y
+sudo gem install fpm
+bash /tmp/linux-on-ibm-z-scripts/Terraform/${version}/build_terraform.sh -y
+tar cvfz terraform-${version}-linux-s390x.tar.gz -C $PWD/bin terraform -C $PWD/src/github.com/hashicorp/terraform LICENSE
+fpm -s tar -t rpm --prefix /usr/bin -n terraform -v ${version} terraform-${version}-linux-s390x.tar.gz
+fpm -s tar -t deb --prefix /usr/bin -n terraform -v ${version} terraform-${version}-linux-s390x.tar.gz
+exit 0
